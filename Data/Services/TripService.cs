b4998f427dace2a6fef93dac7a06ce992c831f45
jessica0522 <jessica040532@gmail.com>
2021-09-10 11:45:52 +1200

    add TripService

---
+++ Data/Services/TripService.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Trips.Data;
+
+namespace Trip.Data
+{
+    public class TripService : ITripService
+    {
+        public void AddTrip(Trips.Data.Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteTrip(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Trips.Data.Trip> GetAllTrips()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Trips.Data.Trip GetTripById(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTrip(int tripId, Trips.Data.Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
